142c0d1b237fbf77b90a69980441b63b234700de
BertramYe <2516559898@qq.com>
2024-12-05 18:24:23 +0800

    add more debug for the middleware

--- middleware.py
+++ middleware.py
@@ -21,8 +21,8 @@ class MiddleWare(BaseHTTPMiddleware):
     
     async def dispatch(self, request, call_next:RequestResponseEndpoint):
         try:
-            print('request',request)
-            # print('headers',request.headers)
+            # print('request',request)
+            print('headers',request.headers)
             # origin = request.headers.get("origin") or request.headers.get("referer")
             # # 获取 X-Forwarded-For 头部（可能包含多个 IP 地址）
             # x_forwarded_for = request.headers.get("X-Forwarded-For")
@@ -30,11 +30,11 @@ class MiddleWare(BaseHTTPMiddleware):
             # print('x_forwarded_for',x_forwarded_for)
             # print('host',request.headers.get('host'))
             print("Content-Type:", request.headers.get('Content-Type'))
-            print("Request body:", await request.body())
-            
+            # body = await request.body() 
             # 请求头验证通过，继续处理请求
             response = await call_next(request)
-            print('response',response)
+            print('response',response.headers)
+            print('response',response.body)
         except Exception as er:
             print('error',er)
         return response

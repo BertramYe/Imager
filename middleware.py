8a51cfac8e311c79573500199d633bf7fb465592
BertramYe <2516559898@qq.com>
2024-12-05 18:03:56 +0800

    add more debug for the middleware

--- middleware.py
+++ middleware.py
@@ -19,14 +19,18 @@ class MiddleWare(CORSMiddleware):
 
     
     async def dispatch(self, request, call_next:RequestResponseEndpoint):
-        print('request',request.headers)
-        origin = request.headers.get("origin") or request.headers.get("referer")
-        # 获取 X-Forwarded-For 头部（可能包含多个 IP 地址）
-        x_forwarded_for = request.headers.get("X-Forwarded-For")
-        print('origin',origin)
-        print('x_forwarded_for',x_forwarded_for)
-        print('host',request.headers.get('host'))
-        
-        # 请求头验证通过，继续处理请求
-        response = await call_next(request)
+        try:
+            print('request',request.headers)
+            origin = request.headers.get("origin") or request.headers.get("referer")
+            # 获取 X-Forwarded-For 头部（可能包含多个 IP 地址）
+            x_forwarded_for = request.headers.get("X-Forwarded-For")
+            print('origin',origin)
+            print('x_forwarded_for',x_forwarded_for)
+            print('host',request.headers.get('host'))
+            
+            # 请求头验证通过，继续处理请求
+            response = await call_next(request)
+            print('response',response)
+        except Exception as er:
+            print('error',er)
         return response
